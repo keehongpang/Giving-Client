b75838b8c5e83a840ac8b7e8f0518a9f5e00ad6a
keehongpang <keehong.pang@northlandchurch.net>
2017-02-06 15:16:08 -0500

    Add files via upload

--- ws/webservice.php
+++ ws/webservice.php
@@ -1,1 +1,25 @@
+<?php 
+    $ch = curl_init();
 
+    $encoded = '';
+    foreach($_POST as $name => $value) {
+        $encoded .= urlencode($name).'='.urlencode($value).'&';
+    }
+    // chop off last ampersand
+    $encoded = substr($encoded, 0, strlen($encoded)-1);
+
+    curl_setopt($ch, CURLOPT_URL, 'http://172.30.5.47/ws/webservices.php?'.$encoded); 
+//    curl_setopt($ch, CURLOPT_URL, 'http://172.30.5.139/ws/webservices.php?'.$encoded); 
+	
+    curl_setopt( $ch, CURLOPT_HEADER, 0);
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
+    curl_setopt( $ch, CURLOPT_HTTPAUTH, 0);
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+	
+    echo json_encode($result);
+//    echo json_encode(json_encode($result));
+
+?>
